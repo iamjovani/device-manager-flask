97796542296537b99a63424599b89458bf14d82b
Jovani Turner <jturner@bertsautoparts.com>
2020-01-08 09:19:30 -0500

    refactoring CRUD

---
+++ model.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+
+
+@app.route('/delete/<string:id_data>', methods = ['GET'])
+def delete(id_data):
+    flash("Record Has Been Deleted Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM students WHERE id=%s", (id_data,))
+    mysql.connection.commit()
+    return redirect(url_for('Index'))
+
+    

0ea4c88e1f1c856c48995c289a6828e68e02d461
iamjovani <zlatanjr08@gmail.com>
2020-02-21 17:25:32 -0500

    file imported successfully based on given format

--- model.py
+++ model.py
@@ -17,26 +17,38 @@ def allowed_file(filename):
 
 
 def importfile(path):
+    
+   x1 = pd.ExcelFile(path) #for sheet name 
+   locations = x1.sheet_names
+   
+   
    df = pd.read_excel (path)
    numOfRow = df.shape[0]
    
+   location = locations[0]
+   #usr      = 'N/A'
+   
    for i in range(numOfRow):
-       name = df.at[i, 'name']
-       serial_number = df.at[i, 'serial number']
-       operating_sys = df.at[i, 'operating system']
-       tablet_type   = df.at[i, 'tablet type']
-       model         = df.at[i, 'model']
-       zone          = df.at[i, 'zone']
-       condtion      = df.at[i, 'condition']
-       date          = df.at[i, 'date']
-       date_damaged  = df.at[i, 'date damaged']
+       name         = df.at[i, 'name']
+       serial_number= df.at[i, 'serial number']
+       operatingsys = df.at[i, 'operating system']
+       devicetype   = df.at[i, 'tablet type']
+       inputmodel   = df.at[i, 'model']
+       inputzone    = df.at[i, 'zone']
+       state        = df.at[i, 'condition']
+       dateadded    = (df.at[i, 'date'])
+       datedamaged  = (df.at[i, 'date damaged'])
+       #user         = df.at[i, 'user']
        
        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
        
-       st = '' # insert statment for imported items goes here
+       st = 'INSERT INTO `devices` (`name`, `serial_number`, `location`, `operating_sys`, `tablet_type`, `model`, `zone`, `state`, `date_added`, `date_damaged`, `user`)' \
+                    'VALUES(\"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\")'.format(
+               name, serial_number, location, operatingsys, devicetype, inputmodel, inputzone, state, dateadded, datedamaged, "NULL") # insert statment for imported items goes here
        
        cur = mysql.connection.cursor()
        cur.execute(st)
        mysql.connection.commit()
    
-       print(name)
+       print(st)
+       #print( x1.sheet_names )
